e820a39772e9b602c3adcb10890594194ab38df9
B. R. Mullikin <ben@brmullikin.com>
2016-07-04 14:24:48 -0500

    Ignore D203 in flake8 cmd

--- cli/commands/cmd_flake8.py
+++ cli/commands/cmd_flake8.py
@@ -21,5 +21,5 @@ def cli(skip_init, path):
     if skip_init:
         flake8_flag_exclude = ' --exclude __init__.py'
 
-    cmd = 'flake8 {0}{1} --ignore=E302'.format(path, flake8_flag_exclude)
+    cmd = 'flake8 {0}{1} --ignore=E302,D203'.format(path, flake8_flag_exclude)
     return subprocess.call(cmd, shell=True)

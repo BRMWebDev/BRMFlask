247b7d0aa26d45d980ade4132972282bf83f267c
B. R. Mullikin <ben@brmullikin.com>
2017-05-10 16:09:42 -0500

    Finish Unit Tests for dotenv

--- brmflask/tests/utils/test_dotenv.py
+++ brmflask/tests/utils/test_dotenv.py
@@ -6,3 +6,5 @@ import os
 def test_dotenv(tmpdir):
     """Should verify that dotenv returns usable values"""
     assert dict(dotenv('file')) == {}
+    # Env file w/ various file conditions for dotenv()
+    assert dict(dotenv('./.brm_env')) == dict(dotenv('./.brm_env'))

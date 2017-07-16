957e78debccf67e1fcfc9da95501696e4cbb9bf4
B. R. Mullikin <ben@brmullikin.com>
2017-07-16 08:58:42 -0500

    - Remove profiler from dry run (use the flask_debug instead)  - Edit the templates

--- dryrun.py
+++ dryrun.py
@@ -1,10 +1,6 @@
 """
 Load app and run it.
-
-Enables debug and profiler.
 """
-from werkzeug.contrib.profiler import ProfilerMiddleware
-
 from brmflask.app import create_app
 
 if __name__ == "__main__":
@@ -12,10 +8,4 @@ if __name__ == "__main__":
         config_override={'DEBUG': True},
         template_folder='tests/dryrun/templates'
     )
-    # # Enabling profiling
-    # runapp.config['PROFILE'] = True
-    # # Get 5 most expensive functions
-    # runapp.wsgi_app = ProfilerMiddleware(
-    #     runapp.wsgi_app, restrictions=[5]
-    # )
     runapp.run()
